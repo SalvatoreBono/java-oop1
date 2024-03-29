9e7a55b180634038826b9d3d8572c617e4c0ef1a
Salvatore Bono <salvatorebono2001@gmail.com>
2024-03-29 18:17:48 +0100

    finish ex 2. to do: finish ex 1

--- src/org/oop/Bank.java
+++ src/org/oop/Bank.java
@@ -7,32 +7,40 @@ public class Bank {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Ciao Inserisci il tuo nome");
         String bankAccount = keyboard.nextLine();
-        System.out.println(bankAccount +" hai 3 opzioni:");
-        System.out.println("1 = Versare");
-        System.out.println("2 = Prelevare");
-        System.out.println("3 = Uscire");
 
         Conto conto1 = new Conto(bankAccount, 50);
 
-        int bankOption = keyboard.nextInt();
+        boolean exit = false;
 
-        //se scegli il numero 1
-        if (bankOption == 1){
-            System.out.println("Quanto vuoi versare?");
-            int money = keyboard.nextInt();
-            conto1.addMoney(money);
-            conto1.info();
-            //se scegli il numero 2
-        } else if (bankOption == 2) {
-            System.out.println("Quanto vuoi prelevare?");
-            int money = keyboard.nextInt();
-            //se il tuo saldo è inferiore ai solddi da prelevare
-            if (conto1.saldo < money){
-                System.out.println("Mi dispiace non puoi prelevare perchè il tuo conto andrà in negativo");
-            } else {
-            conto1.removeMoney(money);
+        //fai questo finchè exit è true
+        do {
+            System.out.println(bankAccount +" hai 3 opzioni:");
+            System.out.println("1 = Versare");
+            System.out.println("2 = Prelevare");
+            System.out.println("3 = Uscire");
+            int bankOption = keyboard.nextInt();
+            //se scegli il numero 1
+            if (bankOption == 1){
+                System.out.println("Quanto vuoi versare?");
+                int money = keyboard.nextInt();
+                conto1.addMoney(money);
+                conto1.info();
+                //se scegli il numero 2
+            } else if (bankOption == 2) {
+                System.out.println("Quanto vuoi prelevare?");
+                int money = keyboard.nextInt();
+                //se il tuo saldo è inferiore ai solddi da prelevare
+                if (conto1.saldo < money){
+                    System.out.println("Mi dispiace non puoi prelevare perchè il tuo conto andrà in negativo");
+                } else {
+                    conto1.removeMoney(money);
+                }
+                conto1.info();
+            } else if (bankOption == 3) {
+                exit = true;
+                System.out.println("Sei uscito");
             }
-            conto1.info();
-        }
+        }while (!exit);
+
     }
 }
