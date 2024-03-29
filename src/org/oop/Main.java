191731608a38d95f34bc0a1f3c41d826b20429dd
Salvatore Bono <salvatorebono2001@gmail.com>
2024-03-29 15:25:25 +0100

    ex 1 finish

--- src/org/oop/Main.java
+++ src/org/oop/Main.java
@@ -2,6 +2,16 @@ package org.oop;
 
 public class Main {
     public static void main(String[] args) {
+    Prodotto prodotto1 = new Prodotto("Zaino Puma", "Unisex",14.99);
+    System.out.println("Il codice del tuo prodotto è: "+prodotto1.codice);
+    System.out.println("Prezzo senza iva: "+ String.format("%.2f",prodotto1.prezzo)+"€");
+    System.out.println("Prezzo con iva: "+String.format("%.2f",prodotto1.getPriceWithIva() )+"€");
+    System.out.println(prodotto1.getCodeAndName());
 
+    Prodotto prodotto2 = new Prodotto("Power Bank", "Tascabile",20);
+    System.out.println("Il codice del tuo prodotto è: "+prodotto2.getCode());
+    System.out.println("Prezzo senza iva: "+ String.format("%.2f",prodotto2.prezzo)+"€");
+    System.out.println("Prezzo con iva: "+String.format("%.2f",prodotto2.getPriceWithIva() )+"€");
+    System.out.println(prodotto2.getCodeAndName());
     }
 }
--- src/org/oop/Prodotto.java
+++ src/org/oop/Prodotto.java
@@ -1,5 +1,41 @@
 package org.oop;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+    Random random = new Random();
+    private String strCode = "";
+    //conversione stringa in int
+    public int codice = Integer.parseInt(getCode());
+    private String nome;
+    private String descrizione;
+    public double prezzo;
+    private final double iva = 0.22;
+
+    public Prodotto(String nome, String descrizione, double prezzo) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+    }
+    public double getPriceWithIva(){
+        return prezzo + prezzo * iva;
+    }
+
+    public String getCode(){
+        //for basato sul numero random
+        for (int i = 0; i < random.nextInt(8); i++) {
+            //genero numero random, converto in stringa e concateno
+            strCode += Integer.toString(random.nextInt(8));
+        }
+        return strCode;
+    }
+    public String getCodeAndName(){
+        if (strCode.length() < 8){
+            String strCodeWithName = 0 +"-"+ nome;
+            return strCodeWithName;
+        }else {
+            String strCodeWithName = strCode +"-"+ nome;
+            return strCodeWithName;
+        }
+    }
 }
